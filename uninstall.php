c616fcbf21c939b4facec104937c3cd4ca926229
Andras Guseo <andras@theeventscalendar.com>
2025-09-01 21:45:12 +0200

    Update the uninstall script

--- uninstall.php
+++ uninstall.php
@@ -15,32 +15,34 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
 }
 
-// Only proceed if Tribe Common is available
+// Bail if Tribe Common is not available.
 if ( ! class_exists( 'Tribe__Settings_Manager' ) ) {
 	return;
 }
 
-// Get the options prefix used by the plugin
-$options_prefix = str_replace( '-', '_', 'tec-labs-default-attendee-fields' ) . '_';
+// Get the options prefix used by the plugin.
+$options_prefix = 'tec_labs_default_attendee_fields_';
 
-// Get all tribe options
+// Get all TEC options.
 $tribe_options = Tribe__Settings_Manager::get_options();
 
+// Bail if there are no options.
 if ( ! is_array( $tribe_options ) ) {
 	return;
 }
 
-// Check if the remove_settings_on_delete option is enabled
+// Check if the remove_settings_on_delete option is enabled.
 $remove_settings_key = $options_prefix . 'remove_settings_on_delete';
 $should_remove = isset( $tribe_options[ $remove_settings_key ] ) && $tribe_options[ $remove_settings_key ];
 
+// Bail if the option is not enabled.
 if ( ! $should_remove ) {
 	return;
 }
 
-// Remove all plugin settings
+// Remove all plugin settings.
 foreach ( $tribe_options as $key => $value ) {
 	if ( 0 === strpos( $key, $options_prefix ) ) {
-		tribe_delete_option( $key );
+		tribe_remove_option( $key );
 	}
 }
